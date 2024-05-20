cdcac8726b8113a095e39b3069d8f212183517f1
jodahyeok <jaden0524@naver.com>
2024-05-20 19:51:11 +0900

    라벨, 테이블레이아웃 설정!

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,7 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
+            tableLayoutPanel1 = new TableLayoutPanel();
+            label1 = new Label();
             SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 2;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.Dock = DockStyle.Bottom;
+            tableLayoutPanel1.Location = new Point(0, 106);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 2;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.Size = new Size(534, 405);
+            tableLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("돋움", 27.75F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            label1.Location = new Point(78, 34);
+            label1.Name = "label1";
+            label1.Size = new Size(385, 37);
+            label1.TabIndex = 1;
+            label1.Text = "점심 메뉴를 고르시오";
             // 
             // Form1
             // 
@@ -36,11 +62,17 @@
             AutoScaleMode = AutoScaleMode.Font;
             AutoSize = true;
             ClientSize = new Size(534, 511);
+            Controls.Add(label1);
+            Controls.Add(tableLayoutPanel1);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TableLayoutPanel tableLayoutPanel1;
+        private Label label1;
     }
 }
